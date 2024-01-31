326e896de6240aa5a8df9dfe1bbcaca977b4455f
John Kim <mkcokr2176@gmail.com>
2024-01-30 20:52:36 -0800

    Update crud operation update part

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   // state
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState({ members: [] });
 
-  useEffect(() => 
-    {
-    	fetch("/members").then(
-          response => response.json()
-        ).then(
-          data => {
-            setData(data);
-            console.log(data);
-          }
-        ).catch(
-          (err) => console.log(err)
-        )
-    }, [])
+  useEffect(() => {
+    fetch("/members")
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
+  const addMember = () => {
+    fetch("/add-member", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setData({ members: [...data.members] });
+      })
+      .catch(err => console.log(err));
+  };
+
+  const deleteMember = () => {
+    fetch("/add-member", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setData({ members: [...data.members] });
+      })
+      .catch(err => console.log(err));
+  };
   return (
     <div className='App'>
-      <h1>testing.</h1>
+      <h1>Testing</h1>
       <div>
-        {(typeof data.members === 'undefined') ? (
-          <p>loding...</p>
+        {typeof data.members === 'undefined' ? (
+          <p>Loading...</p>
         ) : (
           data.members.map((member, i) => (
             <p key={i}>{member}</p>
           ))
         )}
+        <button onClick={addMember}>Add Member</button>
+        <button onClick={deleteMember}>Delete Member</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
